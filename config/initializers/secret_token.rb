81970fbd46a79d6abf691d8dcd8b73ee4925a196
Oleksandr Bondar <obondar@gmail.com>
2011-10-02 10:14:12 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PictureWall::Application.config.secret_token = '4a301eb5297ca763e94ceac1dc6d8ba60b2c3707ac684f155b3eef1626d25975cf3c7873cc3b06b4483d8c6f2b9f3b31a42c24b7dbdd81ebc76c1aba0bc8da82'
